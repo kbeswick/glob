ec4004095b9571a29b17b3e730df881123bcc195
kbeswick <kevinbeswick00@gmail.com>
2010-10-21 18:00:03 -0400

    add model for topics

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
   has_many :comments, :dependent => :destroy
+  has_many :topics
+
+  accepts_nested_attributes_for :topics, :allow_destroy => :true, :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101021214002) do
+ActiveRecord::Schema.define(:version => 20101021215040) do
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(:version => 20101021214002) do
     t.string   "title"
     t.text     "content"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "topics", :force => true do |t|
+    t.string   "name"
+    t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
