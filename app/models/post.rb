1cf5246dc83b6d2c0c17342048ad2768d32ca7e8
kbeswick <kevinbeswick00@gmail.com>
2010-11-04 12:58:24 -0400

    create better post links -- use paramaterized title and post id

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ActiveRecord::Base
   has_many :topics
 
   accepts_nested_attributes_for :topics, :allow_destroy => :true, :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
+
+  def to_param
+    "#{id.to_s}-#{title.parameterize}"
+  end
 end
