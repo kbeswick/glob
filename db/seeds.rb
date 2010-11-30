cc7f01a25fd64d5de8eee420cfc3c7f21a41c001
Kevin Beswick <kevinbeswick00@gmail.com>
2010-11-30 18:33:08 +0000

    add initial admin user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+User.create(:username => 'admin', :email => 'admin@admin.com', :password => 'admin', :password_confirmation = 'admin')
