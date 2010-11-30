cc458cdbc12f885c2e18d6bdc853088f83b6af7e
Kevin Beswick <kevinbeswick00@gmail.com>
2010-11-30 18:41:02 +0000

    typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-User.create(:username => 'admin', :email => 'admin@admin.com', :password => 'admin', :password_confirmation = 'admin')
+User.create(:username => 'admin', :email => 'admin@admin.com', :password => 'admin', :password_confirmation => 'admin')
